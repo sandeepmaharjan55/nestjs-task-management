fd72886f8aaf436b931820a3a53e527b1577bdca
sandeepmaharjan55 <sandeepmaharjan55@gmail.com>
2020-04-21 15:37:04 +0545

    custom validation pipe for Statuses

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task-dto';
@@ -26,10 +26,15 @@ export class TasksService {
     return tasks;
   }
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+    if (!found) {
+      throw new NotFoundException('Task with ID "${id}" not found');
+    }
+    return found;
   }
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id != id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id != found.id);
   }
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
@@ -45,6 +50,7 @@ export class TasksService {
   updateTaskStatus(id: string, status: TaskStatus) {
     const task = this.getTaskById(id);
     task.status = status;
+
     return task;
   }
 }
