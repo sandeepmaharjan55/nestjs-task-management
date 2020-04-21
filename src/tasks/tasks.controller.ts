c7648efe1703305d5efa0a433b9c7815b6c6e509
sandeepmaharjan55 <sandeepmaharjan55@gmail.com>
2020-04-21 14:51:02 +0545

    added validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,8 @@ import {
   Delete,
   Patch,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
@@ -33,6 +35,7 @@ export class TasksController {
     return this.tasksService.deleteTaskById(id);
   }
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
   }
